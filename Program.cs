430f0a946f53e72b43cd7c3cb387225c207364ee
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2023-03-09 10:29:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿char[] alphabet = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
+
+string encryptedLink = "rddzc://mvkccbyyw.qsdrel.myw/k/njQi4uvX";
+string decryptedLink = "";
+
+for (int i = 0; i < encryptedLink.Length; i++)
+{
+    char decryptedChar;
+
+    if (!alphabet.Contains(Char.ToLower(encryptedLink[i]))) { decryptedChar = encryptedLink[i]; }
+    else
+    {
+        int index = Array.IndexOf(alphabet, Char.ToLower(encryptedLink[i]));
+        decryptedChar = alphabet[(26 + index - 10) % 26];
+    }
+    
+    decryptedLink += Char.IsUpper(encryptedLink[i]) ? Char.ToUpper(decryptedChar) : decryptedChar;
+}
+
+Console.WriteLine(decryptedLink);
